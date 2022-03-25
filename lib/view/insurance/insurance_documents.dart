96f088fb588afce364f8be1b0a52f4e3403bd572
shrijeet punewar <shrijeet100@gmail.com>
2022-03-25 12:41:16 +0530

    bugs resolved / merged

--- lib/view/insurance/insurance_documents.dart
+++ lib/view/insurance/insurance_documents.dart
@@ -1,23 +1,24 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:esys_flutter_share_plus/esys_flutter_share_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:health_bloom/model/request/request.dart';
 import 'package:health_bloom/view/insurance/widgets/insaurance_card.dart';
-import 'package:http/http.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
-import 'package:share/share.dart';
-import 'dart:io';
+
 import '../../components/textbuilder.dart';
 import '../../model/response/response.dart';
 import '../../model/response/search_insurance_response.dart';
 import '../../services/api/repository/auth_repository.dart';
 import '../../utils/colors.dart';
-import '../../utils/downloader.dart';
 import '../../utils/loading.dart';
 import '../../utils/text_field/custom_text_field.dart';
 
 class InsuranceDocuments extends StatefulWidget {
   final SearchInsuranceResponseDatum insurance;
-  const InsuranceDocuments({Key key,this.insurance}) : super(key: key);
+  const InsuranceDocuments({Key key, this.insurance}) : super(key: key);
 
   @override
   State<InsuranceDocuments> createState() => _InsuranceDocumentsState();
@@ -44,7 +45,7 @@ class _InsuranceDocumentsState extends State<InsuranceDocuments> {
         selectedStartDate = picked;
       });
       _fromDate.text =
-      "${selectedStartDate.day}-${selectedStartDate.month}-${selectedStartDate.year}";
+          "${selectedStartDate.day}-${selectedStartDate.month}-${selectedStartDate.year}";
       getDocumentsFamily();
     }
   }
@@ -67,7 +68,7 @@ class _InsuranceDocumentsState extends State<InsuranceDocuments> {
         ));
       }
       _toDate.text =
-      "${selectedEndDate.day}-${selectedEndDate.month}-${selectedEndDate.year}";
+          "${selectedEndDate.day}-${selectedEndDate.month}-${selectedEndDate.year}";
       getDocumentsFamily();
     }
   }
@@ -82,59 +83,50 @@ class _InsuranceDocumentsState extends State<InsuranceDocuments> {
     GetDocumentsFamilyRequest _request = GetDocumentsFamilyRequest(
         fromDate: selectedStartDate ?? null,
         toDate: selectedEndDate ?? null,
-        patient: widget.insurance.patient.id
-    );
+        patient: widget.insurance.patient.id);
     print(_request.toJson().toString());
-    GetDocumentsFamilyResponse _response = await adminAPI.getDocumentsFamilyAPI(_request);
+    GetDocumentsFamilyResponse _response =
+        await adminAPI.getDocumentsFamilyAPI(_request);
 
     _currentResponse = _response;
 
     _currentResponse.data.report.forEach((element) {
-      _sortedDocs.add(
-          GetDocumentsFamilyResponseCommonObject(
-            id: element.id,
-            name: element.name,
-            date: element.date,
-            description: element.description,
-            images: element.reportImage,
-            patient: element.patient,
-            userId: element.userId,
-            type: "Report"
-          )
-      );
+      _sortedDocs.add(GetDocumentsFamilyResponseCommonObject(
+          id: element.id,
+          name: element.name,
+          date: element.date,
+          description: element.description,
+          images: element.reportImage,
+          patient: element.patient,
+          userId: element.userId,
+          type: "Report"));
     });
 
     _currentResponse.data.prescription.forEach((element) {
-      _sortedDocs.add(
-          GetDocumentsFamilyResponseCommonObject(
-              id: element.id,
-              name: element.doctorName,
-              date: element.consultationDate,
-              description: element.doctorAdvice,
-              images: element.prescriptionImage,
-              patient: element.patient,
-              userId: element.userId,
-              clinicName: element.clinicName,
-              userAilment: element.userAilment,
-              type: "Prescription"
-          )
-      );
+      _sortedDocs.add(GetDocumentsFamilyResponseCommonObject(
+          id: element.id,
+          name: element.doctorName,
+          date: element.consultationDate,
+          description: element.doctorAdvice,
+          images: element.prescriptionImage,
+          patient: element.patient,
+          userId: element.userId,
+          clinicName: element.clinicName,
+          userAilment: element.userAilment,
+          type: "Prescription"));
     });
 
     _currentResponse.data.bill.forEach((element) {
-      _sortedDocs.add(
-          GetDocumentsFamilyResponseCommonObject(
-              id: element.id,
-              name: element.name,
-              date: element.date,
-              description: element.description,
-              images: element.billImage,
-              patient: element.patient,
-              userId: element.userId,
-              amount: element.amount,
-              type: "Bill"
-          )
-      );
+      _sortedDocs.add(GetDocumentsFamilyResponseCommonObject(
+          id: element.id,
+          name: element.name,
+          date: element.date,
+          description: element.description,
+          images: element.billImage,
+          patient: element.patient,
+          userId: element.userId,
+          amount: element.amount,
+          type: "Bill"));
     });
 
     await Future.delayed(Duration(seconds: 1));
@@ -155,7 +147,7 @@ class _InsuranceDocumentsState extends State<InsuranceDocuments> {
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
-          onPressed: (){
+          onPressed: () {
             Navigator.pop(context);
           },
         ),
@@ -170,143 +162,157 @@ class _InsuranceDocumentsState extends State<InsuranceDocuments> {
         centerTitle: true,
       ),
       backgroundColor: kWhite,
-      body: _currentResponse != null ? Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              height: 300,
-              width: double.infinity,
-              child: Image.asset(
-                "assets/images/medical_bill.jpg",
-                fit: BoxFit.cover,
-                color: Colors.black45,
-                colorBlendMode: BlendMode.hardLight,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            left: 0,
-            top: 100,
-            child: Container(
-              padding: EdgeInsets.only(top: 40, left: 24, right: 24),
-              decoration: BoxDecoration(
-                  color: kWhite,
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(30),
-                      topLeft: Radius.circular(30))),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: CustomTextField(
-                          readOnly: true,
-                          maxLines: 1,
-                          controller: _fromDate,
-                          label: "Start Date",
-                          textInputType: TextInputType.text,
-                          onChanged: (val) {},
-                          onTap: () {
-                            _startDate(context);
-                          },
-                        ),
-                      ),
-                      const SizedBox(width: 20.0),
-                      Expanded(
-                        child: CustomTextField(
-                          readOnly: true,
-                          maxLines: 1,
-                          controller: _toDate,
-                          label: "End Date",
-                          textInputType: TextInputType.text,
-                          onChanged: (val) {},
-                          onTap: () {
-                            _endDate(context);
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 24.0),
-                  Expanded(
-                    child: ListView.builder(
-                      padding: EdgeInsets.all(0),
-                      itemCount: _sortedDocs.length,
-                      itemBuilder: (context,index){
-                        return InsuranceCard(
-                          nameOfBill: "${_sortedDocs[index].type}: " + _sortedDocs[index].name,
-                          nameOfPatients: _sortedDocs[index].patient.name,
-                          dateOfBill: _sortedDocs[index].date,
-                          avatar: _sortedDocs[index].patient.avatar ?? '',
-                          onTap: (bool v) {
-                            if(v){
-                              _shareIds.add(_sortedDocs[index].id);
-                            }else{
-                              _shareIds.remove(_sortedDocs[index].id);
-                            }
-                            setState(() {});
-                          },
-                          edit: () {},
-                          delete: () {},
-                          showIcons: false,
-                        );
-                      },
+      body: _currentResponse != null
+          ? Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    height: 300,
+                    width: double.infinity,
+                    child: Image.asset(
+                      "assets/images/medical_bill.jpg",
+                      fit: BoxFit.cover,
+                      color: Colors.black45,
+                      colorBlendMode: BlendMode.hardLight,
                     ),
                   ),
-                  SizedBox(height: 16),
-                ],
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  left: 0,
+                  top: 100,
+                  child: Container(
+                    padding: EdgeInsets.only(top: 40, left: 24, right: 24),
+                    decoration: BoxDecoration(
+                        color: kWhite,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(30),
+                            topLeft: Radius.circular(30))),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              child: CustomTextField(
+                                readOnly: true,
+                                maxLines: 1,
+                                controller: _fromDate,
+                                label: "Start Date",
+                                textInputType: TextInputType.text,
+                                onChanged: (val) {},
+                                onTap: () {
+                                  _startDate(context);
+                                },
+                              ),
+                            ),
+                            const SizedBox(width: 20.0),
+                            Expanded(
+                              child: CustomTextField(
+                                readOnly: true,
+                                maxLines: 1,
+                                controller: _toDate,
+                                label: "End Date",
+                                textInputType: TextInputType.text,
+                                onChanged: (val) {},
+                                onTap: () {
+                                  _endDate(context);
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 24.0),
+                        Expanded(
+                          child: ListView.builder(
+                            padding: EdgeInsets.all(0),
+                            itemCount: _sortedDocs.length,
+                            itemBuilder: (context, index) {
+                              return InsuranceCard(
+                                nameOfBill: "${_sortedDocs[index].type}: " +
+                                    _sortedDocs[index].name,
+                                nameOfPatients: _sortedDocs[index].patient.name,
+                                dateOfBill: _sortedDocs[index].date,
+                                avatar: _sortedDocs[index].patient.avatar ?? '',
+                                onTap: (bool v) {
+                                  if (v) {
+                                    _shareIds.add(_sortedDocs[index].id);
+                                  } else {
+                                    _shareIds.remove(_sortedDocs[index].id);
+                                  }
+                                  setState(() {});
+                                },
+                                edit: () {},
+                                delete: () {},
+                                showIcons: false,
+                              );
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                      ],
+                    ),
+                  ),
+                ),
+                if (_loading) LoadingWidget(color: kGrey7,)
+              ],
+            )
+          : LoadingWidget(),
+      floatingActionButton: _shareIds.isNotEmpty
+          ? FloatingActionButton(
+              backgroundColor: kGrey7,
+              child: Icon(
+                Icons.share,
+                color: kWhite,
+                size: 26,
               ),
-            ),
-          ),
-          if (_loading) LoadingWidget()
-        ],
-      ) : LoadingWidget(),
-      floatingActionButton: _shareIds.isNotEmpty ? FloatingActionButton(
-        backgroundColor: kGrey7,
-        child: Icon(Icons.share,color: kWhite,size: 26,),
-        onPressed: () async{
-          String _temp = "";
+              onPressed: () async {
+                setState(() {
+                  _loading = true;
+                });
+                String _temp = "";
+                Map<String, List<int>> _files = {};
 
-          _shareIds.forEach((element) async{
-            List<GetDocumentsFamilyResponseCommonObject> _tempList = _sortedDocs.where((e) => element == e.id).toList();
+                for(String element in _shareIds){
+                  List<GetDocumentsFamilyResponseCommonObject> _tempList =
+                  _sortedDocs.where((e) => element == e.id).toList();
 
-            // var response = await get(Uri.parse(_tempList[0].images[0].assetUrl));
-            // final documentDirectory = (await getExternalStorageDirectory()).path;
-            // File imgFile = new File('$documentDirectory/${_tempList[0].images[0].assetName}');
-            // imgFile.writeAsBytesSync(response.bodyBytes);
-            //
-            // Share.shareFiles(['$documentDirectory/${_tempList[0].images[0].assetName}'],
-            //     subject: 'URL conversion + Share',
-            //     text: 'Hey! Checkout the Share Files repo');
-            //
-            // // String path = await FileDownloader().download(_tempList[0].images[0].assetName, _tempList[0].images[0].assetUrl);
-            // // print("**************************");
-            // // print(path);
-            // // print("**************************");
-            // // Share.shareFiles([path], text: 'Great picture');
+                  _temp = _temp + _tempList[0].name;
+                  _temp = _temp + "\n";
+                  _temp = _temp + _tempList[0].patient.name;
+                  _temp = _temp + "\n";
+                  _temp = _temp + _tempList[0].description;
+                  _temp = _temp + "\n";
+                  _temp = _temp + "\n";
 
-            _temp = _temp + _tempList[0].name;
-            _temp = _temp + "\n";
-            _temp = _temp + _tempList[0].patient.name;
-            _temp = _temp + "\n";
-            _temp = _temp + _tempList[0].description;
-            _temp = _temp + "\n";
-            if(_tempList[0].images.isNotEmpty){
-              _temp = _temp + "Files : ${_tempList[0].images.map((e) => e.assetUrl).toList()}";
-              _temp = _temp + "\n";
-            }
-            _temp = _temp + "\n";
-            _temp = _temp + "\n";
-          });
+                  for(var element in _tempList[0].images){
+                    var request =
+                    await HttpClient().getUrl(Uri.parse(element.assetUrl));
+                    var response = await request.close();
+                    Uint8List bytes =
+                    await consolidateHttpClientResponseBytes(response);
 
-          Share.share(_temp);
-        },
-      ) : null,
+                    _files[element.assetName] = bytes;
+                  }
+
+                }
+
+                await Share.files('Files', _files, '*/*', text: _temp);
+                setState(() {
+                  _loading = false;
+                });
+              },
+            )
+          : null,
     );
   }
+}
+
+class ShareFilesTemp {
+  String name;
+  Uint8List file;
+  ShareFilesTemp({this.name, this.file});
 }
